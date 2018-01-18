c0575e1987696ec17923b13cde4fd40ca4cb667a
Joe <j.saltenberger@gmail.com>
2018-01-18 15:39:32 -0800

    Add default props to Header component.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ class IndecisionApp extends React.Component {
     this.handleAddOption = this.handleAddOption.bind(this);
 
     this.state = {
-      options: ['one', 'two', 'three']
+      options: props.options
     };
   }
 
@@ -41,12 +41,11 @@ class IndecisionApp extends React.Component {
   }
 
   render() {
-    const title = 'Indecision';
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle}/>
+        <Header subtitle={subtitle}/>
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -63,14 +62,22 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+  options: []
+}
+
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>  
   );
 };
+
+Header.defaultProps = {
+  title: 'Indecision'
+}
 
 const Action = (props) => {
   return (
