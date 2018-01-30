2311756d6b5c0a91614c1f7226f57b148eb6de66
Joe <j.saltenberger@gmail.com>
2018-01-30 10:04:09 -0800

    Style the Options list.

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,15 +3,18 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button
-      className="button button--link"
-      onClick={props.handleDeleteOptions}
-    >
-      Remove All
-    </button>
-    {props.options.length === 0 && <p>Add an option to get started.</p>}
-    <ol>
-      { props.options.map((option) => (
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+      </button>
+    </div>
+    {props.options.length === 0 && <p className="widget__message">Add an option to get started.</p>}
+    {
+      props.options.map((option) => (
         <Option
           key={option}
           optionText={option}
@@ -19,7 +22,6 @@ const Options = (props) => (
         />
       ))
     }
-    </ol>
   </div>  
 )
 
