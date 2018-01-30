c00ee8fd86c868aabc540c1200aa34dce536f80a
Joe <j.saltenberger@gmail.com>
2018-01-30 09:42:08 -0800

    Style buttons

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,10 +4,11 @@ const Option = (props) => (
   <div>
     {props.optionText}
     <button
+      className="button button--link"
       onClick={(e) => {
         props.handleDeleteOption(props.optionText);
       }}>
-      remove
+        Remove
     </button>
   </div>  
 )
