65156a22f6dfb90976dbc8542634911cd0ef4a53
Joe <j.saltenberger@gmail.com>
2018-02-28 14:16:01 -0800

    Create build in docs folder.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   module: {
@@ -22,6 +22,6 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'docs')
   }
 };
